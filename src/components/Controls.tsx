44f78d6b3282a4f171b26f878f235579c50d8a12
Eray Aydin <erraydin@gmail.com>
2021-05-24 04:59:56 -0500

    refactoring works

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,9 +1,26 @@
+import { FunctionComponent, useState } from "react";
+import { myP5 } from "../p5/sketch";
 import classes from "./Controls.module.css";
 
-const Controls = () => {
+export type ControlProps = {
+    myP5: React.MutableRefObject<myP5 | null>
+}
+const Controls: FunctionComponent<ControlProps> = ({ myP5 }) => {
+    const [playButtonText, setPlayButtonText] = useState("Play");
+
+    const playButtonHandler = () => {
+        myP5.current!.playing = !myP5.current!.playing;
+        if (myP5.current!.playing) setPlayButtonText("Pause");
+        else setPlayButtonText("Play");
+    }
+
     return (
         <div className={classes.row}>
-            <button className={classes.playButton}>Play</button>
+            <button
+                className={classes.playButton}
+                onClick={playButtonHandler}>
+                {playButtonText}
+            </button>
         </div>
     );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,35 @@
-import Canvas from "./Canvas";
+import { useEffect, useReducer, useRef } from "react";
+import playReducer from "../reducers/playReducer";
+import p5 from "p5";
+import defineSketch, { myP5 } from "../p5/sketch";
 import Controls from "./Controls";
 import classes from "./Main.module.css";
 import Patterns from "./Patterns";
 import Preferences from "./Preferences";
 
 const Main = () => {
+    // const [playing, dispatchPlaying] = useReducer(playReducer, false);
+
+    const wrapper = useRef<HTMLDivElement>(null);
+    const myP5 = useRef<myP5 | null>(null);
+
+    useEffect(() => {
+        if (wrapper.current !== null) {
+            myP5.current = new p5(defineSketch(false, 600 / 30, 30, 30, 20), wrapper.current)
+            console.log("useEffectCalled");
+        }
+    }, [])
+
+
     return (
         <div className={classes.mainContainer}>
-            <Canvas />
+            <div className={classes.canvas} ref={wrapper} />
             <div className={classes.column}>
                 <div className={classes.row}>
                     <Preferences />
                     <Patterns />
                 </div>
-                <Controls />
-
+                <Controls myP5={myP5} />
             </div>
         </div>
     );
